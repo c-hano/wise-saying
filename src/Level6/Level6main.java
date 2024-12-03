d7d334f5bc2be5f5e6c9e068b2ab98d872ca974f
chanho <wdq0304@gmail.com>
2024-12-03 15:23:24 +0900

    Level6

--- src/Level6/Level6main.java
+++ src/Level6/Level6main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Level5main {
+public class Level6main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Words> wordsList = new ArrayList<>();
