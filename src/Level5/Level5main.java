7305fc3b3a3dcee0952ad117dad12e8120c2ddb3
chanho <wdq0304@gmail.com>
2024-12-03 14:47:11 +0900

    Level 5

--- src/Level5/Level5main.java
+++ src/Level5/Level5main.java
@@ -1,4 +1,46 @@
 package Level5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Level5main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Words> wordsList = new ArrayList<>();
+        int count = 1;
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.println("명령 ) ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("등록")) {
+                System.out.println("명언 : ");
+                String words = sc.nextLine().trim();
+                System.out.println("작자 : ");
+                String author = sc.nextLine().trim();
+
+                if (author.isEmpty()) {
+                    author = "작자미상";
+                }
+
+                wordsList.add(new Words(author, words, count));
+
+                System.out.println(count + "번 명언이 등록되었습니다.");
+                count++;
+
+            } else if (command.equals("종료")) {
+                break;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-------------------");
+                for (Words words : wordsList) {
+                    System.out.println(words.countNumber + " / " + words.author + " / " + words.comment);
+                }
+            } else {
+                System.out.println("잘못된 접근 입니다.");
+            }
+        }
+    }
 }
--- src/Level5/Words.java
+++ src/Level5/Words.java
@@ -1,4 +1,14 @@
 package Level5;
 
 public class Words {
+
+    String author;
+    String comment;
+    int countNumber;
+
+    public Words(String author, String comment, int countNumber) {
+        this.author = author;
+        this.comment = comment;
+        this.countNumber = countNumber;
+    }
 }
