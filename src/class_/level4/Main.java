94cd0b131e46bc656340c0474740f0516e9708ee
c-hano <wdq0304@naver.com>
2024-12-11 15:27:55 +0900

    level4

---
+++ src/class_/level4/Main.java
@@ -0,0 +1,38 @@
+package class_.level4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+    }
+}
+
+class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            if(cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+                System.out.println(count+ "번 명언이 등록되었습니다.");
+                count++;
+            }
+
+
+        }
+
+        scanner.close();
+    }
+}
