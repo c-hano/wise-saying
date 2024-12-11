4249ccce80b58dd776ed062f1633dfc02ecb3b16
c-hano <wdq0304@naver.com>
2024-12-11 17:24:59 +0900

    level7

---
+++ src/class_/level7/Main.java
@@ -0,0 +1,12 @@
+package class_.level7;
+
+import class_.level7.com.ll.App;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+    }
+}
+
---
+++ src/class_/level7/com/ll/WiseSaying.java
@@ -0,0 +1,30 @@
+package class_.level7.com.ll;
+
+public class WiseSaying {
+
+
+        private final int id;
+        private final String content;
+        private final String author;
+
+        public WiseSaying(int id, String content, String author) {
+            this.id = id;
+            this.content = content;
+            this.author = author;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+
+    }
+
