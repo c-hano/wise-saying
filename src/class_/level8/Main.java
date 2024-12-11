251af8cf0efd7d4e88850d6af1b2d6bcef5f0416
c-hano <wdq0304@naver.com>
2024-12-11 17:47:56 +0900

    level8

---
+++ src/class_/level8/Main.java
@@ -0,0 +1,12 @@
+package class_.level8;
+
+import class_.level8.com.ll.App;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+    }
+}
+
