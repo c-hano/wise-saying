ff606a015a72135246ccc3b5e92eb124644a7a55
c-hano <wdq0304@naver.com>
2024-12-11 15:59:32 +0900

    level5

--- src/class_/level5/Main.java
+++ src/class_/level5/Main.java
@@ -13,42 +13,49 @@ public class Main {
 }
 
 class App {
+    // 인스턴스 변수
+
+    Scanner scanner = new Scanner(System.in);
+    int count = 1;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
+
     public void run() {
         System.out.println("== 명언 앱 ==");
-        Scanner scanner = new Scanner(System.in);
-        int count = 1;
-        List<WiseSaying> wiseSayings = new ArrayList<>();
+
 
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                wiseSayings.add(new WiseSaying(count, content, author));
-
-                System.out.println(count+ "번 명언이 등록되었습니다.");
-                count++;
+                actionAdd();
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------");
 
-                for(WiseSaying words : wiseSayings) {
+                for (WiseSaying words : wiseSayings) {
                     System.out.println("%d / %s / %s".formatted(words.getId(), words.getAuthor(), words.getContent()));
 
                 }
             }
 
-
         }
 
         scanner.close();
+    }
+
+    void actionAdd(){
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        wiseSayings.add(new WiseSaying(count, content, author));
+
+        System.out.println(count + "번 명언이 등록되었습니다.");
+        count++;
     }
 }
 
@@ -63,6 +70,7 @@ class WiseSaying {
         this.content = content;
         this.author = author;
     }
+
     public int getId() {
         return id;
     }
