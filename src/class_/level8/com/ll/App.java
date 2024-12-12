93dfd13450648f8421a2375b12f71f949ef4d098
c-hano <wdq0304@naver.com>
2024-12-12 12:47:54 +0900

    Creat SystemController

--- src/class_/level8/com/ll/App.java
+++ src/class_/level8/com/ll/App.java
@@ -1,6 +1,7 @@
 package class_.level8.com.ll;
 
 
+import class_.level8.com.ll.domain.system.controller.SystemController;
 import class_.level8.com.ll.domain.wiseSaying.controller.WiseSayingController;
 import class_.level8.com.ll.domain.wiseSaying.entity.WiseSaying;
 
@@ -9,9 +10,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private final Scanner scanner = new Scanner(System.in);
-    private final WiseSayingController wiseSayingController = new WiseSayingController(scanner);
+    private final Scanner scanner;
+    private final WiseSayingController wiseSayingController;
+    private final SystemController systemController;
 
+    public App() {
+        this.scanner = new Scanner(System.in);
+        this.wiseSayingController = new WiseSayingController(scanner);
+        this.systemController = new SystemController();
+        //생성자를 굳이 쓰는 이유? -> 후에 추가 될때 파라미터를 사용하기 용이하다.
+    }
     public void run() {
         System.out.println("== 명언 앱 ==");
 
@@ -21,6 +29,7 @@ public class App {
             String cmd = scanner.nextLine();
 
             if (cmd.equals("종료")) {
+                systemController.actionExit();
                 break;
             } else if (cmd.equals("등록")) {
                 wiseSayingController.actionAdd();
